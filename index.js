e3d12d267373d62350b5343828b3d49e6326cae8
fitDizzle <markfitnessaz@gmail.com>
2022-04-13 23:36:12 -0700

    .... still trying

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.get('/', (req, res) => {
+    res.json({message: 'WELCOME!!!!'})
+})
+
 
 //add database here
 require('./database/otherConnection');
