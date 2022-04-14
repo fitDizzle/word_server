1228c481de43cad1b17c3fbfe63c5b59e2562d62
fitDizzle <markfitnessaz@gmail.com>
2022-04-13 22:11:30 -0700

    updated config and other connection files

--- database/otherConnection.js
+++ database/otherConnection.js
@@ -4,11 +4,20 @@ const Sequelize = require("sequelize");
 const { host, user, password, database } = config.development;
 
 const credentials = {
-  user,
-  host,
+  user: "jlwafmgpcrpzcw",
+  host: "ec2-34-207-12-160.compute-1.amazonaws.com",
   database: "scrabble_words",
-  password: "",
+  password: "f62a583125324e6eaf2d2e3261c6321549acfab53ab4a5cb01ea7afc0676f077",
+  uri: "postgres://jlwafmgpcrpzcw:f62a583125324e6eaf2d2e3261c6321549acfab53ab4a5cb01ea7afc0676f077@ec2-34-207-12-160.compute-1.amazonaws.com:5432/dd9bjopg329br2",
   port: 5432,
+  dialectOptions:{
+    allowPublicKeyRetrieval: true,
+    skipSetTimeZone: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 };
 
 async function poolDemo() {
