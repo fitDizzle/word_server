3fbebc47c8fd95affbcd4f61790117ea4f2d0299
Mark Clark <mark.clark@gusto.com>
2023-10-09 00:18:54 -0700

    fix: remove data leak

--- database/otherConnection.js
+++ database/otherConnection.js
@@ -4,12 +4,6 @@ const Sequelize = require("sequelize");
 const { host, user, password, database } = config.development;
 
 const credentials = {
-  user: "rgunrhgljdfqss",
-  host: "ec2-34-236-103-63.compute-1.amazonaws.com",
-  database: "d75iq9upr8bbj4",
-  password: "8d58565f1ba6ca59807b76d5280654e55a7613d1662922197afcfd4e77e6e206",
-  uri: "postgres://rgunrhgljdfqss:8d58565f1ba6ca59807b76d5280654e55a7613d1662922197afcfd4e77e6e206@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d75iq9upr8bbj4",
-  port: 5432,
   dialectOptions: {
     allowPublicKeyRetrieval: true,
     skipSetTimeZone: true,
