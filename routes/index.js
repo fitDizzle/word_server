9c30da37756e01f0f95d9f4ee03031cff97cbc25
Mark Clark <mark.clark@gusto.com>
2023-10-08 23:05:41 -0700

    feat: minor cleanup remove extra empty lines etc

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,4 @@ const router = require('express').Router();
 router.use('/gameplay/word', require("./wordRoutes"));
 router.use("/search", require("./wordSearchRoutes"));
 
-
 module.exports = router;
--- routes/wordSearchRoutes.js
+++ routes/wordSearchRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const wordSearchController = require("../controllers/wordSearchController");
 
-router.route("/relative/:letters").get(wordSearchController.findRelativeWords); // Get suggested Words
-router.route("/filter/:letters").get(wordSearchController.getAllWords); // Get suggested Words
+router.route("/relative/:letters").get(wordSearchController.findRelativeWords);
+router.route("/filter/:letters").get(wordSearchController.getAllWords);
 
 module.exports = router;
