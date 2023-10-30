4255d22c08f9bb41c7b93271967cdc56d0a8cd32
Mark Clark <mark.clark@gusto.com>
2023-10-29 23:11:02 -0700

    fix: remove leaked data and replace with env vars

--- database/otherConnection.js
+++ database/otherConnection.js
@@ -1,14 +1,14 @@
 const { Pool, Client } = require("pg");
 const config = require("../database/config/config.json");
 const Sequelize = require("sequelize");
-const { host, user, password, database } = config.development;
+// const { host, user, password, database } = config.development;
 
 const credentials = {
-  user: "amizcydpfrkjrd",
-  host: "ec2-52-206-36-147.compute-1.amazonaws.com",
-  database: "dbbkkauj2qrq9a",
-  password: "e455af1385fd0baeff9f60c9494a3734921e362d1c9fbc7d63818014557338dd",
-  uri: process.env.uri,
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  uri: process.env.DATABASE_URI,
   port: 5432,
   dialectOptions: {
     allowPublicKeyRetrieval: true,
