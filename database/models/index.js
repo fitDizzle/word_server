eb9bd0c6d4f2a9b7a8241c6f956bc75ee84989b2
Mark Clark <mark.clark@gusto.com>
2023-11-02 00:04:51 -0700

    fix: remove xtra setup

--- database/models/index.js
+++ database/models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const { applyExtraSetup } = require('./extra-setup');
+// const { applyExtraSetup } = require('./extra-setup');
 
 // In a real app, you should keep the database connection URL as an environment variable.
 // But for this example, we will just use a local SQLite database.
