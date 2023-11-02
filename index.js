574a6c6dc6785e33954795c11e1a1763c5095431
Mark Clark <mark.clark@gusto.com>
2023-11-01 23:18:17 -0700

    test: still testing

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
+    console.log('server up');
     res.json({ message: 'request received' })
 })
 
