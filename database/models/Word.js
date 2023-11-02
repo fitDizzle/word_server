839af176596d23f69bc547e49e3f62b2fb789f85
Mark Clark <mark.clark@gusto.com>
2023-11-02 00:43:53 -0700

    fix: chnges

--- database/models/Word.js
+++ database/models/Word.js
@@ -2,40 +2,9 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  // class Word extends Model {
-  //   /**
-  //    * Helper method for defining associations.
-  //    * This method is not a part of Sequelize lifecycle.
-  //    * The `models/index` file will call this method automatically.
-  //    */
-  //   static associate(models) {
-  //     // define association here
-  //   }
-  // }
-  // Word.init(
-  //   {
-  //     word: DataTypes.STRING,
-  //     lower_range: DataTypes.STRING,
-  //     upper_range: DataTypes.STRING,
-  //   },
-  //   {
-  //     sequelize,
-  //     modelName: "Word",
-  //   }
-  // );
-  // return Word;
-
   sequelize.define('word', {
-    // The following specification of the 'id' attribute could be omitted
-    // since it is the default.
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    word: {
-      type: DataTypes.STRING,
-    }
+    word: DataTypes.STRING,
+    lower_range: DataTypes.STRING,
+    upper_range: DataTypes.STRING,
   });
 };
--- database/models/index.js
+++ database/models/index.js
@@ -1,9 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-// const { applyExtraSetup } = require('./extra-setup');
 
-// In a real app, you should keep the database connection URL as an environment variable.
-// But for this example, we will just use a local SQLite database.
-// const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.DB_HOST,
@@ -14,16 +10,4 @@ const sequelize = new Sequelize({
   benchmark: true
 });
 
-const Word = sequelize.define('Word', {
-  // Define your Word model properties here
-  word: DataTypes.STRING,
-  lower_range: DataTypes.STRING,
-  upper_range: DataTypes.STRING,
-});
-
-// We define all models according to their files.
-for (const modelDefiner of modelDefiners) {
-  modelDefiner(sequelize);
-}
-
-module.exports = { sequelize, Word };
+module.exports = { sequelize };
