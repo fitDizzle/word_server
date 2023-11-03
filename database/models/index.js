76175fa27c2988db0d5b0103bb966b1ca14b6de0
Mark Clark <mark.clark@gusto.com>
2023-11-02 23:58:08 -0700

    test new env var

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
-const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const db = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
   host: process.env.HOST,
   dialect: 'postgres',
   dialectOptions: {
