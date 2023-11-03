e74ffcfad4e7559efa1f89d0c77bce10842be6e8
Mark Clark <mark.clark@gusto.com>
2023-11-02 23:12:18 -0700

    feat: update word model

--- database/models/Word.js
+++ database/models/Word.js
@@ -1,20 +1,39 @@
 "use strict";
 const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL);
 
-module.exports = (sequelize, DataTypes) => {
-  class Word extends Model { }
+// module.exports = (sequelize, DataTypes) => {
+//   class Word extends Model { }
 
-  Word.init(
-    {
-      word: DataTypes.STRING,
-      lower_range: DataTypes.STRING,
-      upper_range: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "Word",
-    }
-  );
-  return Word;
-};
+//   Word.init(
+//     {
+//       word: DataTypes.STRING,
+//       lower_range: DataTypes.STRING,
+//       upper_range: DataTypes.STRING,
+//     },
+//     {
+//       sequelize,
+//       modelName: "Word",
+//     }
+//   );
+//   return Word;
+// };
+
+const Word = db.define('Word', {
+  word: {
+    type: DataTypes.STRING,
+  },
+  lower_range: {
+    type: DataTypes.STRING,
+  },
+  upper_range: {
+    type: DataTypes.STRING,
+  },
+},
+  {
+    db,
+    modelName: "Word",
+  }
+);
+
+module.exports = { Word };
--- database/models/index.js
+++ database/models/index.js
@@ -1,14 +1,10 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+
+const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
   dialect: 'postgres',
   logging: false,
   ssl: { rejectUnauthorized: false }
 });
 
-try {
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+module.exports = { db };
