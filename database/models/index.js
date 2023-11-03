19d3ae1cf6f19f8a57a7eb679f842b004a9630e0
Mark Clark <mark.clark@gusto.com>
2023-11-03 00:33:17 -0700

    feat: add seeder to index

--- database/models/index.js
+++ database/models/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
+const words = require("../../word-list.json");
 
 async function initializeDatabase() {
   try {
@@ -40,6 +41,39 @@ async function initializeDatabase() {
     } catch (error) {
       console.log('Error creating Word model: ', error);
     }
+
+    const importData = async () => {
+      try {
+        let largeIndex = 0;
+
+        const mapAndSave = async (arr) => {
+          let lower = arr[0].slice(0, 2);
+          let upper = arr[arr.length - 1].slice(0, 2);
+
+          let toCreate = await arr.map((word) => {
+            return {
+              word,
+              lower_range: lower,
+              upper_range: upper,
+            };
+          });
+          await Word.bulkCreate(toCreate, { validate: true });
+
+          largeIndex += 999;
+        };
+        do await mapAndSave(words.slice(largeIndex, largeIndex + 1000));
+        while (largeIndex <= words.length);
+        console.log("Success");
+        process.exit();
+      } catch (error) {
+        // basic error here
+        console.log("error");
+        console.log(error);
+        process.exit(1);
+      }
+    };
+
+    importData();
 
     return db;
   } catch (error) {
