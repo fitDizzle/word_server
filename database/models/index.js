c2c017feb57b90ca7cb393228ca94e19d06250d2
Mark Clark <mark.clark@gusto.com>
2023-11-02 23:39:40 -0700

    adding dotenv

--- database/models/index.js
+++ database/models/index.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config()
 
 const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
